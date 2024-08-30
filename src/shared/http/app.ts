ec58442d389cf9badde9b14dc9a0f2f599c1a6f1
lucas carvalho <lucas.araripina@hotmail.com>
2024-08-29 22:28:02 -0300

    configs inicial routes

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -5,12 +5,13 @@ import cors from 'cors';
 
 import { errors } from 'celebrate';
 import AppError from '@shared/errors/appError';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
-
+app.use(routes);
 app.use(errors());
 
 app.use(
