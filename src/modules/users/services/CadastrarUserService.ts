3c8bdd89381bfb9f98ba3d7dff8db26b17592ac2
lucas carvalho <lucas.araripina@hotmail.com>
2024-08-31 17:09:01 -0300

    adding  createuser service

---
+++ src/modules/users/services/CadastrarUserService.ts
@@ -0,0 +1,62 @@
+import AppError from '@shared/errors/appError';
+import { UsersRepository } from '../typeorm/repositories/usersRepository';
+import User from '../typeorm/entities/user';
+import { hash } from 'bcryptjs';
+enum Role {
+  gestante = 'gestante',
+  medico = 'medico',
+}
+interface MedicoDTO {
+  nome: string;
+  email: string;
+  password: string;
+  role: Role;
+  crm: string;
+}
+interface Gestante2DTO {
+  nome: string;
+  email: string;
+  password: string;
+  role: Role;
+  dataNascimento: Date;
+}
+class CadastrarUserService {
+  private usersRepository = UsersRepository;
+
+  public async execute(data: MedicoDTO | Gestante2DTO): Promise<User> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email,
+    );
+
+    if (userAlreadyExists) {
+      throw new AppError('User already exists');
+    }
+    const hashedPassword = await hash(data.password, 8);
+    let user;
+    if (data.role == 'gestante') {
+      const gestanteData = data as Gestante2DTO;
+      user = this.usersRepository.create({
+        nome: gestanteData.nome,
+        email: gestanteData.email,
+        role: gestanteData.role,
+        dataNascimento: gestanteData.dataNascimento,
+        password: hashedPassword,
+      });
+    } else {
+      const medicoData = data as MedicoDTO;
+      user = this.usersRepository.create({
+        nome: medicoData.nome,
+        email: medicoData.email,
+        role: medicoData.role,
+        crm: medicoData.crm,
+        password: hashedPassword,
+      });
+    }
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CadastrarUserService;
--- src/modules/users/typeorm/entities/user.ts
+++ src/modules/users/typeorm/entities/user.ts
@@ -1,5 +1,13 @@
 import Glicemia from '@modules/glicemias/typeorm/entities/glicemia';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 class User {
@@ -18,8 +26,26 @@ class User {
   @Column({ default: 'gestante' })
   role: string;
 
+  @Column({ nullable: true })
+  crm: string;
+
+  @Column({ nullable: true })
+  dataNascimento: Date;
+
+  @ManyToOne(() => User, gestante => gestante.gestantes)
+  medico: User;
+
+  @OneToMany(() => User, gestante => gestante.medico)
+  gestantes: User[];
+
   @OneToMany(() => Glicemia, glicemia => glicemia.gestante)
   glicemias: Glicemia[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   @Column({ default: true })
   isActive: boolean;
