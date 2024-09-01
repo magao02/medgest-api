6863f4ada99f61aa6eb2e2d4e7e4833ddf85e468
lucas carvalho <lucas.araripina@hotmail.com>
2024-09-01 11:39:42 -0300

    adding  glicemiaslistar service

---
+++ src/modules/glicemias/services/ListarGlicemiaService.ts
@@ -0,0 +1,21 @@
+import Glicemia from '../typeorm/entities/glicemia';
+import { GlicemiasRepository } from '../typeorm/repositories/glicemiasRepository';
+
+class ListarGlicemiaService {
+  private glicemiasRepository = GlicemiasRepository;
+
+  public async execute(gestante_id: string): Promise<Glicemia[]> {
+    const gestante =
+      await this.glicemiasRepository.findByGestanteId(gestante_id);
+
+    if (!gestante) {
+      throw new Error('Gestante not found');
+    }
+    const glicemias =
+      await this.glicemiasRepository.findByGestanteId(gestante_id);
+
+    return glicemias;
+  }
+}
+
+export default ListarGlicemiaService;
