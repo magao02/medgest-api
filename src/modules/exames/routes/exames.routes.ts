b4cfebce4e012d683ca061cd62b364c8b946f2a2
lucas carvalho <lucas.araripina@hotmail.com>
2024-09-01 17:30:37 -0300

    feat: adding Exame Router

---
+++ src/modules/exames/routes/exames.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import uploadConfig from '@config/upload';
+import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+
+import ExamesController from '../controllers/examesController';
+
+const examesRouter = Router();
+const examesController = new ExamesController();
+const upload = multer(uploadConfig);
+
+examesRouter.get('/', examesController.index);
+
+examesRouter.post(
+  '/',
+  upload.single('arquivo'),
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+      data: Joi.date().required(),
+      gestante_id: Joi.string().uuid().required(),
+    },
+  }),
+  examesController.create,
+);
+
+examesRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  examesController.delete,
+);
+
+export default examesRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 import glicemiasRouter from '@modules/glicemias/routes/glicemias.routes';
 import pesosRouter from '@modules/pesos/routes/pesos.routes';
 import loginRouter from '@modules/users/routes/logins.routes';
+import examesRouter from '@modules/exames/routes/exames.routes';
 
 const routes = Router();
 
@@ -14,6 +15,7 @@ routes.get('/', (req, res) => {
 routes.use('/users', usersRouter);
 routes.use('/glicemias', glicemiasRouter);
 routes.use('/pesos', pesosRouter);
+routes.use('/exames', examesRouter);
 routes.use('/login', loginRouter);
 
 export default routes;
